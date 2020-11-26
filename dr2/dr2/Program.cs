682796e46f2784ea82b4a9d7b6628a76c44684c8
[mmestrovic7] <[marijamestrovic47@gmail.com]>
2020-11-26 21:55:36 +0100

    basic menu and if branches

--- dr2/dr2/Program.cs
+++ dr2/dr2/Program.cs
@@ -6,7 +6,36 @@ namespace dr2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Odaberite akciju:\n1 - Ispis cijele liste\n2 - Ispis imena pjesme unosom pripadajućeg rednog broja\n3 - Ispis rednog broja pjesme unosom pripadajućeg imena");
+            Console.WriteLine("4 - Unos nove pjesme\n5 - Brisanje pjesme po rednom broju\n6 - Brisanje pjesme po imenu\n7 - Brisanje cijele liste");
+            Console.WriteLine("8 - Uređivanje imena pjesme\n9 - Uređivanje rednog broja pjesme, odnosno premještanje pjesme na novi redni broj u listi\n0 - Izlaz iz aplikacije");
+            var a = int.Parse(Console.ReadLine());
+
+            if (a == 0)
+                ;
+            else if (a == 1)
+                ;
+            else if (a == 2)
+                ;
+            else if (a == 3)
+                ;
+            else if (a == 4)
+                ;
+            else if (a == 5)
+                ;
+            else if (a == 6)
+                ;
+            else if (a == 7)
+                ;
+            else if (a == 8)
+                ;
+            else if (a == 9)
+                ;
+            else
+                Console.WriteLine("Neispravan unos");
+
+
+
         }
     }
 }
